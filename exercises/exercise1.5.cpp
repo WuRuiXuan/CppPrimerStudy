f97a5020c07a3ad3a0fcbb0b16b12e3e9f972595
WuRuiXuan <740461697@qq.com>
2022-09-25 21:27:48 +0800

    first commit

---
+++ exercises/exercise1.5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Remind yourself";
+    std::cout << " that it's okay";
+    std::cout << " not to be perfect.";
+    std::cout << std::endl;
+    return 0;
+}
---
+++ exercises/exercise1.7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+/*
+ * /* 包含不正确的嵌套注释 */
+ */
+int main() {
+    printf("观察编译器返回的错误信息");
+    return 0;
+}
